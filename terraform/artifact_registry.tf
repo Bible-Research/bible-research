3e1f2715ccd6c51a7ad516cd1bb592b1b12dbf64
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2026-05-08 08:26:18 +0300

    chore(terraform): reduce Artifact Registry image retention from 6 to 2 versions

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -6,12 +6,12 @@ resource "google_artifact_registry_repository" "gae_standard" {
   description   = "Repository to store images related to App Engine Standard deployments."
   format        = "DOCKER"
 
-  # Retain the six newest image versions; remove untagged artifacts.
+  # Retain the two newest image versions; remove untagged artifacts.
   cleanup_policies {
-    id     = "keep-latest-6"
+    id     = "keep-latest-2"
     action = "KEEP"
     most_recent_versions {
-      keep_count = 6
+      keep_count = 2
     }
   }
 
@@ -43,10 +43,10 @@ resource "google_artifact_registry_repository" "audio_generator" {
   format        = "DOCKER"
 
   cleanup_policies {
-    id     = "keep-latest-6"
+    id     = "keep-latest-2"
     action = "KEEP"
     most_recent_versions {
-      keep_count = 6
+      keep_count = 2
     }
   }
 
